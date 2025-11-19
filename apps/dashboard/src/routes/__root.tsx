dde2ad47cb65d5628ebe0c2b6336d935df28d114
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2025-11-19 15:23:48 -0300

    feat [dash]: add initial files

---
+++ apps/dashboard/src/routes/__root.tsx
@@ -0,0 +1,62 @@
+/// <reference types="vite/client" />
+
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import type { ReactNode } from "react";
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "Sailor",
+			},
+		],
+	}),
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<RootDocument>
+			<Outlet />{" "}
+			<TanStackDevtools
+				config={{
+					position: "bottom-right",
+				}}
+				plugins={[
+					{
+						name: "Tanstack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+				]}
+			/>
+		</RootDocument>
+	);
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
+}
---
+++ apps/dashboard/vite.config.ts
@@ -0,0 +1,18 @@
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	plugins: [
+		devtools(),
+		tsConfigPaths(),
+		tanstackStart(),
+		nitro({
+			preset: "bun",
+		}),
+		viteReact(),
+	],
+});
